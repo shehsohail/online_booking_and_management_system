49f85a49ce7b9c7d7c863ee5ca1b2d9623d4de0a
danlingsun <36872351+danlingsun@users.noreply.github.com>
2018-02-26 22:55:49 -0500

    Update City_of_Airports.sql

--- City_of_Airports.sql
+++ City_of_Airports.sql
@@ -1,4 +1,4 @@
-'CREATE TABLE `City_of_Airports` (
+CREATE TABLE `City_of_Airports` (
   `City_Market_ID` int(11) NOT NULL,
   `City_Name` varchar(128) DEFAULT NULL,
   PRIMARY KEY (`City_Market_ID`)
