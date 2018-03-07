f64f772b64ff9f71d4d105970ce2fa293e753e8b
shehsohail <ss2467@scarletmail.rutgers.edu>
2018-03-06 23:48:06 -0500

    Delete Airports.sql

--- Airports.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE `Airports` (
-  `Airport_Code` varchar(5) NOT NULL,
-  `Airport_Name` varchar(128) DEFAULT NULL,
-  PRIMARY KEY (`Airport_Code`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1'
