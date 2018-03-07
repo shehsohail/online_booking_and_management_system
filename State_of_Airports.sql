b1ae1716b066e2c6e0a1fc2877a12915b81428c1
shehsohail <ss2467@scarletmail.rutgers.edu>
2018-03-06 23:48:34 -0500

    Delete State_of_Airports.sql

--- State_of_Airports.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE `State_of_Airports` (
-  `State_Code` varchar(5) NOT NULL,
-  `State_Name` varchar(64) DEFAULT NULL,
-  PRIMARY KEY (`State_Code`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1'
